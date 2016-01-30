facc1d9705033f28a94bc6d46fe15029cec5236f
Kevin Faherty <k@enemyspeak.com>
2016-01-30 16:02:26 -0600

    menu scene has particles.

---
+++ Demonomicon/Assets/Scripts/particleHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class particleHandler : MonoBehaviour {
+
+	public Transform particle;
+
+	// Use this for initialization
+	void Start () {
+		int particleLimit = 35;
+		for (int i = 0; i < particleLimit; i++) {
+            Instantiate(particle,new Vector3(0, 0, 0), Quaternion.identity);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
