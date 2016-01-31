fc6ec9ce0704f4efc006271677e9d22d018d4306
Jaegersama <jaeger121us@gmail.com>
2016-01-31 00:30:51 -0600

    commited

--- Demonomicon/Assets/Scripts/Battle.cs
+++ Demonomicon/Assets/Scripts/Battle.cs
@@ -23,4 +23,17 @@ public class Battle : MonoBehaviour {
 	
 		return null;//new Demon(null);
 	}
-}
+
+	public void Attack(Skill attack){
+		//float adv = 1.0f
+		//if(((ally.type+1)%3)==enemy.type)
+		//then adv = 1.2f    
+		//else if(((enemy.type+1)%3 == ally.type)
+		//then adv = .8f
+		//float offense = ally.offense*skill.multiplier*adv*def(reduce by percent)
+		//convert to int
+		//subtract from hp
+		//if(hp<=0){next fighter, if next fighter = null, EndBattle}
+		//adjust temperment, check for betrayel
+	}
+} 
