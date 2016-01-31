b5a540c08c56c8e26aaf73904b1b99d6366bb56c
Kevin Faherty <k@enemyspeak.com>
2016-01-30 21:41:50 -0600

    logo

---
+++ Demonomicon/Assets/titleSin.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class titleSin : MonoBehaviour {
+
+	private float t = 0;
+	private Vector2 initialPosition;
+
+	// Use this for initialization
+	void Start () {
+		initialPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		t += Time.deltaTime;
+		transform.position = initialPosition + new Vector2(0,Mathf.Sin(t/2)/3);;
+	}
+}
